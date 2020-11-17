af996200fcd11d7665787898e9e4e18ba3661f01
Denis Kisina <kisinad@gmail.com>
2020-11-17 05:05:41 -0600

    refactor the permutate algo prob

--- src/main/java/io/denis/kisina/strings/Permutate.java
+++ src/main/java/io/denis/kisina/strings/Permutate.java
@@ -1,2 +1,26 @@
-package io.denis.kisina.strings;public class Permutate {
+package io.denis.kisina.strings;
+
+public class Permutate {
+
+    public static void permutate(String str){
+        permutation("", str);
+    }
+
+    private static void permutation(String ans, String str) {
+        int n = str.length();
+
+        if(n == 0){
+            System.out.println(ans);
+
+        }else{
+            for(int i=0; i < n; i++){
+
+                permutation(ans + str.charAt(i), str.substring(0, i) + str.substring(i+1, n));
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        permutate("abc");
+    }
 }
